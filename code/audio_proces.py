da9f014903304eab87de9d88c03ba1415ba8fa4d
lyz <1515685918@qq.com>
2020-12-16 15:50:44 +0800

    Finish audio_process

--- code/audio_proces.py
+++ code/audio_proces.py
@@ -1,7 +1,6 @@
 import pickle as pkl
-
-import librosa
 import numpy as np
+import librosa
 
 SR_DOWN = 11000
 
@@ -9,9 +8,10 @@ SR_DOWN = 11000
 def audio_process(path):
     """
     按照论文中的方式对声音数据进行预处理
+    - 没有做log transform
     """
     data = pkl.load(open(path, 'rb'))
-    audio = data['audio']
+    audio = data['audio'].T
     sr = data['audio_samplerate']
 
     stft_data = []
@@ -20,9 +20,23 @@ def audio_process(path):
         a_stft = librosa.stft(a_rs, n_fft=510, hop_length=128)
         stft_data.append(a_stft)
 
+    return stft_data
+
 
 def main():
-    audio_process('data/audio_data.pkl')
+    import matplotlib.pyplot as plt
+    from librosa.display import specshow
+
+    stft = audio_process('data/train/toy_elephant/0/audio_data.pkl')
+
+    # display stft result
+    fig, ax = plt.subplots(nrows=4, ncols=1, sharex=True)
+    for i in range(4):
+        D = librosa.amplitude_to_db(np.abs(stft[i]), ref=np.max)
+        img = specshow(D, y_axis='linear', x_axis='time', sr=SR_DOWN, ax=ax[i])
+        ax[i].label_outer()
+    fig.colorbar(img, ax=ax, format="%+2.f dB")
+    plt.show()
 
 
 if __name__ == '__main__':
