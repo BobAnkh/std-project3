7abd2256a6452e80e1ad48c767ce47dd527c67b8
BobAnkh <bobankhshen@gmail.com>
2020-12-26 18:32:09 +0800

    feat(test): add test codes for task3

--- test.py
+++ test.py
@@ -84,7 +84,7 @@ def test_task2(root_path):
             map(
                 lambda i: {
                     "label": sample["label"][i],
-                    "img_class": preds[i].detach().cpu().numpy().tolist()
+                    "class": preds[i].detach().cpu().numpy().tolist()
                 }, range(len(sample['label']))))
         image_rel.append(tmp)
 
@@ -105,7 +105,7 @@ def test_task2(root_path):
             map(
                 lambda i: {
                     "label": sample["label"][i],
-                    "audio_class": preds[i].detach().cpu().numpy().tolist()
+                    "class": preds[i].detach().cpu().numpy().tolist()
                 }, range(len(sample['label']))))
         audio_rel.append(mapper)
 
@@ -122,7 +122,7 @@ def test_task2(root_path):
             map(
                 lambda i: {
                     "label": sample["label"][i],
-                    "angle": outputs[i].detach().cpu().numpy().tolist()
+                    "angle_net": outputs[i].detach().cpu().numpy().tolist()
                 }, range(len(sample['label']))))
         angle_rel.append(tmp)
 
@@ -138,10 +138,9 @@ def test_task2(root_path):
             map(
                 lambda i: {
                     "label": sample["label"][i],
-                    "pos": outputs[i].detach().cpu().numpy().tolist()
+                    "pos_net": outputs[i].detach().cpu().numpy().tolist()
                 }, range(len(sample['label']))))
         loc_rel.append(tmp)
-
     print('Start matching...')
     angle_rel = pd.json_normalize(sum(angle_rel, []))
     audio_rel = pd.json_normalize(sum(audio_rel, []))
@@ -178,13 +177,111 @@ def test_task3(root_path):
     '''
     print('Pre-processing audio data!')
     pre_process(root_path)
-    results = None
+
+    print('Processing mask data...')
+    mask_list = test_mask_process(root_path)
+
+    print('Image classification...')
+    image_rel = []
+    test_data1 = VideoTestDataset(root_path)
+    test_loader1 = DataLoader(
+        test_data1, batch_size=64, num_workers=os.cpu_count())
+    model1 = ImageClassifier().load_from_checkpoint('weights/image-resnet44.ckpt')
+    model1.cuda()
+    model1.eval()
+    for sample in test_loader1:
+        inputs = sample["rgb"].float().cuda()
+        outputs = model1(inputs)
+        _, preds = torch.max(outputs, 1)
+        tmp = list(
+            map(
+                lambda i: {
+                    "label": sample["label"][i],
+                    "class": preds[i].detach().cpu().numpy().tolist()
+                }, range(len(sample['label']))))
+        image_rel.append(tmp)
+
+    print('Audio classification...')
+    audio_rel = []
+    test_data2 = AudioTestDataset(root_path)
+    test_loader2 = DataLoader(test_data2,
+                              batch_size=1,
+                              num_workers=os.cpu_count())
+    model2 = AudioClassifier().load_from_checkpoint('weights/audio-resnet20.ckpt')
+    model2.cuda()
+    model2.eval()
+    for sample in test_loader2:
+        inputs = sample["audio"].float().cuda()
+        outputs = model2(inputs)
+        _, preds = torch.max(outputs, 1)
+        mapper = list(
+            map(
+                lambda i: {
+                    "label": sample["label"][i],
+                    "class": preds[i].detach().cpu().numpy().tolist()
+                }, range(len(sample['label']))))
+        audio_rel.append(mapper)
+
+    print('Angle calculated...')
+    angle_rel = []
+    model3 = ActionAngle().load_from_checkpoint(
+        'weights/action-angle-resnet110.ckpt')
+    model3.cuda()
+    model3.eval()
+    for sample in test_loader2:
+        inputs = sample["audio"].float().cuda()
+        outputs = model3(inputs)
+        tmp = list(
+            map(
+                lambda i: {
+                    "label": sample["label"][i],
+                    "angle_net": outputs[i].detach().cpu().numpy().tolist()
+                }, range(len(sample['label']))))
+        angle_rel.append(tmp)
+
+    print('Location calculated...')
+    loc_rel = []
+    model4 = ActionLoc().load_from_checkpoint('weights/action-loc-resnet110.ckpt')
+    model4.cuda()
+    model4.eval()
+    for sample in test_loader2:
+        inputs = sample["audio"].float().cuda()
+        outputs = model4(inputs)
+        tmp = list(
+            map(
+                lambda i: {
+                    "label": sample["label"][i],
+                    "pos_net": outputs[i].detach().cpu().numpy().tolist()
+                }, range(len(sample['label']))))
+        loc_rel.append(tmp)
+    print('Start matching...')
+    angle_rel = pd.json_normalize(sum(angle_rel, []))
+    audio_rel = pd.json_normalize(sum(audio_rel, []))
+    image_rel = pd.json_normalize(sum(image_rel, []))
+    loc_rel = pd.json_normalize(sum(loc_rel, []))
+    mask_list = pd.json_normalize(mask_list)
+
+    audio_info = pd.merge(pd.merge(audio_rel, angle_rel, on='label', how='outer'),
+                          loc_rel,
+                          on='label',
+                          how='outer')
+    video_info = pd.merge(image_rel, mask_list, on='label', how='outer')
+    info_list = pd.merge(audio_info, video_info, on='class', how='outer')
+
+    classes = info_list.groupby(['class'])
+    info_classes = [classes.get_group(i).to_dict('records') for i in range(10)]
+    results = [find_match(d) for d in info_classes]
+    results = sorted(sum(results, []))
+    results = dict(list(map(
+        lambda i: (i[0], int(i[1].split('_')[-1])),
+        results
+    )))
+    for l in test_data2.data_dir:
+        results.setdefault(l["label"], -1)
     return results
 
 
 if __name__ == "__main__":
-    task1 = test_task1("./dataset/task1/test")
+    test_task1("./dataset/task1/test")
     test_task2("./dataset/task2/test/0")
-    json.dump(task1,
-              open('task1_results.json', 'w', encoding='utf-8'),
-              ensure_ascii=False)
+    test_task3("./dataset/task3/test/0")
